64a9b8b3865d9bc74fef3fd4ed496d1bfd5f3b39
mramirid <amir.rhythm@gmail.com>
2019-10-20 09:57:04 +0700

    Added files

--- app/src/androidTest/java/com/mramirid/moviecatalogue/utils/FakeDataDummy.java
+++ app/src/androidTest/java/com/mramirid/moviecatalogue/utils/FakeDataDummy.java
@@ -17,7 +17,8 @@ public class FakeDataDummy {
 				"During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
 				"EN",
 				"2019",
-				8.8f / 2
+				8.8f / 2,
+				false
 		);
 	}
 
@@ -31,7 +32,8 @@ public class FakeDataDummy {
 				"Set in Springfield, the average American town, the show focuses on the antics and everyday adventures of the Simpson family; Homer, Marge, Bart, Lisa and Maggie, as well as a virtual cast of thousands. Since the beginning, the series has been a pop culture icon, attracting hundreds of celebrities to guest star. The show has also made name for itself in its fearless satirical take on politics, media and American life in general.",
 				"EN",
 				"1989",
-				7.1f / 2
+				7.1f / 2,
+				false
 		);
 	}
 }
