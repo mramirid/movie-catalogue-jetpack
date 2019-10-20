2f9a17afba56737e1e51f28519662e249460222d
mramirid <amir.rhythm@gmail.com>
2019-10-20 09:59:38 +0700

    Added files

---
+++ app/src/test/java/com/mramirid/moviecatalogue/utils/InstantAppExecutors.java
@@ -0,0 +1,12 @@
+package com.mramirid.moviecatalogue.utils;
+
+import java.util.concurrent.Executor;
+
+public class InstantAppExecutors extends AppExecutors {
+
+	private static Executor instant = Runnable::run;
+
+	public InstantAppExecutors() {
+		super(instant, instant, instant);
+	}
+}
